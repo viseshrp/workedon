a287258db912cb8eb25f7cd03ce2260fa029b443
viseshrp <viseshrprasad@gmail.com>
2021-10-12 16:24:21 -0400

    add some default commands

--- workedon/cli.py
+++ workedon/cli.py
@@ -1,13 +1,16 @@
 """Console script for workedon."""
 import sys
+
 import click
 
+from . import __version__
 
-@click.command()
+
+@click.command(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.version_option(__version__, "-v", "--version")
 def main(args=None):
     """Console script for workedon."""
-    click.echo("Replace this message by putting your code into "
-               "workedon.cli.main")
+    click.echo("Replace this message by putting your code into " "workedon.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
 
