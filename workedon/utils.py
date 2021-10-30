b646049bbec910ed7207502f3f43c1d635ca6ffa
viseshrp <viseshrprasad@gmail.com>
2021-10-30 19:14:44 -0400

    use generated sha1 from uuid as identifier

--- workedon/utils.py
+++ workedon/utils.py
@@ -1,23 +1,7 @@
-import sys
-from functools import wraps
-
-import click
-
-from .conf import settings
-
-if sys.version_info >= (3, 9):
-    import zoneinfo
-else:
-    from backports import zoneinfo
+import hashlib
+import uuid
 
 
-def pretty_print_work(func):
-    @wraps(func)
-    def pretty(*args, **kwargs):
-        work = func(*args, **kwargs)
-        click.secho(f"\nID: {work.uuid}", fg="green")
-        user_time = work.created.astimezone(zoneinfo.ZoneInfo(settings.user_tz))
-        click.echo(f"Date: {user_time.strftime(f'{settings.DATETIME_FORMAT}')}\n")
-        click.secho(f"    {work.work}\n", bold=True, fg="white")
-
-    return pretty
+def get_unique_hash():
+    unique_id = str(uuid.uuid4()).encode('utf-8')
+    return hashlib.sha1(unique_id).hexdigest()
