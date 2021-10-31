9a6fc4b5598e362056e6bfba929247d8a4737cb6
viseshrp <viseshrprasad@gmail.com>
2021-10-31 18:01:25 -0400

    add handler and tz utils

--- workedon/utils.py
+++ workedon/utils.py
@@ -1,7 +1,35 @@
 import hashlib
+import sys
 import uuid
+from functools import wraps
+
+import click
+
+from .conf import settings
+
+if sys.version_info >= (3, 9):
+    import zoneinfo
+else:
+    from backports import zoneinfo
+
+
+def to_internal_tz(date_time):
+    return date_time.astimezone(zoneinfo.ZoneInfo(settings.internal_tz))
 
 
 def get_unique_hash():
     unique_id = str(uuid.uuid4()).encode('utf-8')
     return hashlib.sha1(unique_id).hexdigest()
+
+
+def click_handler(func):
+    @wraps(func)
+    def handle(*args, **kwargs):
+        try:
+            from .conf import settings
+            settings.configure()
+            func(*args, **kwargs)
+        except Exception as e:
+            raise click.ClickException(click.style(str(e), fg='bright_red'))
+
+    return handle
