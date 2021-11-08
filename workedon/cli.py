20fecd864bf0be8af10b9f0da1685d4b3e652e3f
viseshrp <viseshrprasad@gmail.com>
2021-11-07 22:37:21 -0500

    add more date filters

--- workedon/cli.py
+++ workedon/cli.py
@@ -60,12 +60,60 @@ def stuff(work):
     type=click.STRING,
     help="end date-time to filter with"
 )
+@click.option(
+    "-d",
+    "--past-day",
+    "period",
+    flag_value="day",
+    is_flag=True,
+    help="Fetch work done in the past 24 hours"
+)
+@click.option(
+    "-w",
+    "--past-week",
+    "period",
+    flag_value="week",
+    is_flag=True,
+    help="Fetch work done in the past week"
+)
+@click.option(
+    "-m",
+    "--past-month",
+    "period",
+    flag_value="month",
+    is_flag=True,
+    help="Fetch work done in the past month"
+)
+@click.option(
+    "-y",
+    "--past-year",
+    "period",
+    flag_value="year",
+    is_flag=True,
+    help="Fetch work done in the past year"
+)
+@click.option(
+    "-e",
+    "--yesterday",
+    "period",
+    flag_value="yesterday",
+    is_flag=True,
+    help="Fetch work done yesterday"
+)
+@click.option(
+    "-o",
+    "--today",
+    "period",
+    flag_value="today",
+    is_flag=True,
+    help="Fetch work done today"
+)
 @command_handler
-def what(start_date, end_date):
+def what(start_date, end_date, period):
     """
     Fetch your saved work
     """
-    fetch_work(start_date, end_date)
+    fetch_work(start_date, end_date, period)
 
 
 if __name__ == "__main__":
--- workedon/workedon.py
+++ workedon/workedon.py
@@ -14,6 +14,8 @@ try:
 except ImportError:
     import zoneinfo
 
+WORK_CHUNK_SIZE = 100
+
 
 def save_work(work):
     """
@@ -39,27 +41,46 @@ def _generate_work(result):
     and yield one at a time for
     display.
     """
-    for work_set in chunked(result, 100):
+    for work_set in chunked(result, WORK_CHUNK_SIZE):
         for work in work_set:
             yield work
 
 
-def fetch_work(start_date, end_date):
+def _get_date_range(start_date, end_date, period):
+    curr_dt = now()
+    start = curr_dt - datetime.timedelta(days=7)
+    end = curr_dt
+    if period:
+        # past week is the default
+        if period == "yesterday":
+            start = parser.parse_datetime("12am yesterday")
+            end = parser.parse_datetime("12am today")
+        elif period == "today":
+            start = parser.parse_datetime("12am today")
+        elif period == "day":  # past 24 hours
+            start = parser.parse_datetime("yesterday")
+        elif period == "month":  # past month
+            start = parser.parse_datetime("1 month ago")
+        elif period == "year":  # past year
+            start = parser.parse_datetime("1 year ago")
+    else:
+        # need a start to avoid fetching everything since
+        # the beginning of time.
+        if not start_date and end_date:
+            raise StartDateAbsentError
+        if start_date:
+            start = parser.parse_datetime(start_date)
+        if end_date:
+            end = parser.parse_datetime(end_date)
+
+    return to_internal_dt(start), to_internal_dt(end)
+
+
+def fetch_work(start_date, end_date, period):
     """
     Fetch saved work filtered based on user input
     """
-    start = parser.parse_datetime(start_date)
-    end = parser.parse_datetime(end_date)
-    if not start and end:
-        raise StartDateAbsentError
-    if not start:
-        # default is 1 week ago
-        start = now() - datetime.timedelta(days=7)
-    if not end:
-        # default is now
-        end = now()
-    start = to_internal_dt(start)
-    end = to_internal_dt(end)
+    start, end = _get_date_range(start_date, end_date, period)
     try:
         with init_db():
             works = Work.select().where((Work.timestamp >= start) & (Work.timestamp <= end)).order_by(
