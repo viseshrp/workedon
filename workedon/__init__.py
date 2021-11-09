d10392b9c73d405bae8f81db8553261887f0d790
viseshrp <viseshrprasad@gmail.com>
2021-11-08 20:08:47 -0500

    0.2.0
    remove version refs in code
    
    Update __init__.py

--- workedon/__init__.py
+++ workedon/__init__.py
@@ -1,6 +1,11 @@
 """Top-level package for workedon."""
 
-__author__ = """Visesh Prasad"""
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__author__ = "Visesh Prasad"
 __email__ = "viseshrprasad@gmail.com"
-__version__ = "0.1.1"
 __name__ = "workedon"
+__version__ = importlib_metadata.version(__name__)
