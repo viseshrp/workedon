24035f7b8dcf657f6c46c79aeb72e555e44dbe52
viseshrp <viseshrprasad@gmail.com>
2021-12-31 19:42:33 -0500

    remove importlib shim

--- workedon/__init__.py
+++ workedon/__init__.py
@@ -1,11 +1,6 @@
 """Top-level package for workedon."""
 
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    import importlib_metadata
-
 __author__ = "Visesh Prasad"
 __email__ = "viseshrprasad@gmail.com"
 __name__ = "workedon"
-__version__ = importlib_metadata.version(__name__)
+__version__ = "0.2.4"
