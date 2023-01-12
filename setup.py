1d4aa99aa0ee93ca06ff11d994cd7e785515730a
viseshrp <visesh.rajendraprasad@ansys.com>
2023-01-12 10:24:33 -0500

    3.11 support

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     url="https://github.com/viseshrp/workedon",
