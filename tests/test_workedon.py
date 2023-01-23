7a5e65a9171c38521d1b4659dcf9947c9e05c5c2
viseshrp <viseshrprasad@gmail.com>
2023-01-22 19:24:11 -0500

    fix main test

--- tests/test_workedon.py
+++ tests/test_workedon.py
@@ -5,31 +5,20 @@
 from click.testing import CliRunner
 import pytest
 
-from workedon import cli, workedon
+from workedon import __main__
 
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def runner():
+    """Fixture for invoking command-line interfaces."""
+    return CliRunner()
 
 
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main)
+    result = runner.invoke(__main__.main)
     assert result.exit_code == 0
-    assert "workedon.cli.main" in result.output
-    help_result = runner.invoke(cli.main, ["--help"])
+    help_result = runner.invoke(__main__.main, ["--help"])
     assert help_result.exit_code == 0
-    assert "--help  Show this message and exit." in help_result.output
+    assert "--help     Show this message and exit" in help_result.output
