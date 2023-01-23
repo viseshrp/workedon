e994738b16cb81422cb82a4396795f9df8c1a264
viseshrp <viseshrprasad@gmail.com>
2023-01-22 19:28:24 -0500

    use fixture for test_cli_main

--- tests/test_workedon.py
+++ tests/test_workedon.py
@@ -14,11 +14,10 @@ def runner():
     return CliRunner()
 
 
-def test_command_line_interface():
+def test_cli_main(runner):
     """Test the CLI."""
-    runner = CliRunner()
     result = runner.invoke(__main__.main)
     assert result.exit_code == 0
     help_result = runner.invoke(__main__.main, ["--help"])
     assert help_result.exit_code == 0
-    assert "--help     Show this message and exit" in help_result.output
+    assert "--help  Show this message and exit." in help_result.output
