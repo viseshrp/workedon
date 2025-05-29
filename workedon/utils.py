0acab89aef1dba3e4bca4a94403d87be872741d3
Visesh Rajendraprasad <viseshrprasad@gmail.com>
2025-05-29 16:37:19 -0400

    Update utils.py

--- workedon/utils.py
+++ workedon/utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from functools import wraps
+from typing import Any, Callable
 import uuid
 import zoneinfo
 
@@ -8,69 +9,66 @@ import click
 from .conf import settings
 
 
-def get_unique_hash():
+def get_unique_hash() -> str:
     """
-    Generate a hash similar to git's commit id
+    Generate a hash similar to git's commit id.
     """
     return uuid.uuid4().hex
 
 
-def to_internal_dt(date_time):
+def to_internal_dt(date_time: datetime) -> datetime:
     """
     Convert input datetime to internal timezone
     and remove the second and microsecond components.
     """
-    return (
-        date_time.astimezone(zoneinfo.ZoneInfo(settings.internal_tz))
-        .replace(second=0)
-        .replace(microsecond=0)
+    return date_time.astimezone(zoneinfo.ZoneInfo(settings.internal_tz)).replace(
+        second=0, microsecond=0
     )
 
 
-def now():
+def now() -> datetime:
     """
-    Current datetime in user's local timezone
+    Current datetime in user's local timezone.
     """
     return datetime.now(zoneinfo.ZoneInfo(settings.TIME_ZONE))
 
 
-def get_default_time():
+def get_default_time() -> datetime:
     """
-    Default internal datetime
+    Default internal datetime.
     """
     return to_internal_dt(now())
 
 
-def load_settings(func):
+def load_settings(func: Callable[..., Any]) -> Callable[..., Any]:
     """
-    Loads settings before a
-    command is run.
+    Decorator that loads settings before a Click command is run.
     """
 
     @wraps(func)
-    def handler(*args, **kwargs):
+    def handler(*args: Any, **kwargs: Any) -> Any:
         try:
-            from .conf import settings
+            from .conf import settings as _settings
 
-            user_settings = {}
+            user_settings: dict[str, Any] = {}
             for key, value in kwargs.items():
                 if key.isupper() and value:
                     user_settings[key] = value
-            settings.configure(user_settings=user_settings)
+            _settings.configure(user_settings=user_settings)
 
-            func(*args, **kwargs)
+            return func(*args, **kwargs)
         except Exception as e:
             raise click.ClickException(click.style(str(e), fg="bright_red")) from e
 
     return handler
 
 
-def add_options(options):
+def add_options(options: list[Callable[..., Any]]) -> Callable[..., Any]:
     """
-    Add a bunch of click options to a command
+    Decorator factory that applies each Click option-decorator in `options`.
     """
 
-    def _add_options(func):
+    def _add_options(func: Callable[..., Any]) -> Callable[..., Any]:
         for option in reversed(options):
             func = option(func)
         return func
